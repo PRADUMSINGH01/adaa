aef22e383b8237fa63d0b9c6d64d8ce6a3ab3608
Pradum Singh <hs947518@gmail.com>
2025-07-23 07:55:29 +0000

    qw

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
 "use client";
-import KurtiGrid from "@/components/KurtiGrid/KurtiGrid";
-import Footer from "@/components/Footer/Footer";
-import CategorySlider from "@/components/CategorySlider/CategorySlider";
-import SlideNavbar from "@/components/SlideNavbar/SlideNabar";
-import BottomNav from "@/components/Nav/BottomNav";
-import ResponsiveImageGrid from "@/components/Banners/ResponsiveImageGrid";
-import BannerSlider from "@/components/Banners/BannerSlider";
+// import KurtiGrid from "@/components/KurtiGrid/KurtiGrid";
+// import Footer from "@/components/Footer/Footer";
+// import CategorySlider from "@/components/CategorySlider/CategorySlider";
+// import SlideNavbar from "@/components/SlideNavbar/SlideNabar";
+// import BottomNav from "@/components/Nav/BottomNav";
+// import ResponsiveImageGrid from "@/components/Banners/ResponsiveImageGrid";
+// import BannerSlider from "@/components/Banners/BannerSlider";
 //import WomenClothingGrid from "@/components/AllProductSlider/AllproductSlider";
 export default function Home() {
   return (
     <>
-      <SlideNavbar />
+      {/* <SlideNavbar />
       <BannerSlider />
       <ResponsiveImageGrid />
       <CategorySlider />
       <KurtiGrid />
       <Footer />
-      <BottomNav />
+      <BottomNav /> */}
     </>
   );
 }
